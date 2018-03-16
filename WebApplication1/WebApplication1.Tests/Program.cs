9606df9fd1289601650f5feb174af4dd75b47047
Ken Willoughby <willoughby975@gmail.com>
2018-03-15 20:48:24 -0500

    Assignment 6 additions

---
+++ WebApplication1/WebApplication1.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/EF/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+
+namespace WebApplication1.EF
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
